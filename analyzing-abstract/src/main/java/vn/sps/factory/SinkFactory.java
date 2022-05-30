a2f437c19d23c6a32b96629c43752cd6cf4a2716
Tran Quang Minh <minh.tran.1@swisspost.com>
2022-05-30 18:44:58 +0700

    implement firehose sink

--- analyzing-abstract/src/main/java/vn/sps/factory/SinkFactory.java
+++ analyzing-abstract/src/main/java/vn/sps/factory/SinkFactory.java
@@ -13,24 +13,39 @@
 package vn.sps.factory;
 
 import java.lang.reflect.InvocationTargetException;
+import java.nio.ByteBuffer;
 import java.util.Properties;
 
 import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 
+import com.amazonaws.services.kinesisanalytics.flink.connectors.exception.SerializationException;
 import com.amazonaws.services.kinesisanalytics.flink.connectors.producer.FlinkKinesisFirehoseProducer;
-import com.amazonaws.services.kinesisanalytics.flink.connectors.serialization.JsonSerializationSchema;
+import com.amazonaws.services.kinesisanalytics.flink.connectors.serialization.KinesisFirehoseSerializationSchema;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public final class SinkFactory {
 
     private SinkFactory() {
     }
     
+    @SuppressWarnings("unchecked")
     public static <T> SinkFunction<T> createFirehoseSink(Properties sinkProperties) {
-
+        final ObjectMapper objectMapper = new ObjectMapper();
         final String deliveryStream = sinkProperties.getProperty("deliveryStream");
-        return new FlinkKinesisFirehoseProducer<>(deliveryStream, new JsonSerializationSchema<>(), sinkProperties);
+        return new FlinkKinesisFirehoseProducer<>(deliveryStream, new KinesisFirehoseSerializationSchema() {
+            @Override
+            public ByteBuffer serialize(Object element) {
+                byte[] bytes = new byte[0];
+                try {
+                    bytes = objectMapper.writeValueAsBytes(element);
+                } catch (Exception e) {
+                    throw new SerializationException("Error serializing JSON message", e);
+                }
+                return ByteBuffer.wrap(bytes);
+            }
+        }, sinkProperties);
     }
     
     @SuppressWarnings({ "rawtypes", "unchecked" })
