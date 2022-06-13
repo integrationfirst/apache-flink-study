358d4082b76dddd353abe35188451d99928957f3
Minh Tran <wisekingtran@gmail.com>
2022-06-13 08:57:04 +0700

    Format the code

--- analyzing-app1/src/main/java/vn/ifa/study/flink/report/DemoDataAnalyzer.java
+++ analyzing-app1/src/main/java/vn/ifa/study/flink/report/DemoDataAnalyzer.java
@@ -44,223 +44,223 @@ import vn.sps.cdipp.serialization.JsonNodeSerializationSchema;
 
 public class DemoDataAnalyzer extends AbstractDataAnalyzer<JsonNode> {
 
-	private static final Logger log = LoggerFactory.getLogger(DemoDataAnalyzer.class);
+    private static final Logger log = LoggerFactory.getLogger(DemoDataAnalyzer.class);
 
-	private static final long serialVersionUID = 4919141782930956120L;
+    private static final long serialVersionUID = 4919141782930956120L;
 
-	public DemoDataAnalyzer(final String[] args) throws IOException {
-		super(args);
-	}
+    public DemoDataAnalyzer(final String[] args) throws IOException {
+        super(args);
+    }
 
-	public void test() throws Exception {
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    public void test() throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		final Properties properties = this.getConfiguration("source");
-		properties.put("deserializer", JsonNodeSerializationSchema.class);
+        final Properties properties = this.getConfiguration("source");
+        properties.put("deserializer", JsonNodeSerializationSchema.class);
 
-		final KafkaSource<JsonNode> kafkaSource = SourceFactory.createKafkaSource(properties);
+        final KafkaSource<JsonNode> kafkaSource = SourceFactory.createKafkaSource(properties);
 
-		final ObjectMapper mapper = new ObjectMapper();
-		final JsonNode jsonNodeJohn = mapper.createObjectNode()
-											.put("name", "John");
-		final JsonNode jsonNodeZbe = mapper	.createObjectNode()
-											.put("name", "Zbe");
-		final JsonNode jsonNodeZico = mapper.createObjectNode()
-											.put("name", "Zico");
+        final ObjectMapper mapper = new ObjectMapper();
+        final JsonNode jsonNodeJohn = mapper.createObjectNode()
+                                            .put("name", "John");
+        final JsonNode jsonNodeZbe = mapper.createObjectNode()
+                                           .put("name", "Zbe");
+        final JsonNode jsonNodeZico = mapper.createObjectNode()
+                                            .put("name", "Zico");
 
 //		final DataStream<JsonNode> dataStream1 = env.fromElements(jsonNodeJohn, jsonNodeZbe, jsonNodeZico)
 //				.assignTimestampsAndWatermarks(waterMark());
-		final DataStream<JsonNode> dataStream1 = env.fromSource(kafkaSource, this.waterMark(), "Kafka")
-													.rebalance()
-													.assignTimestampsAndWatermarks(this.waterMark());
+        final DataStream<JsonNode> mainStream = env.fromSource(kafkaSource, this.waterMark(), "Kafka")
+                                                   .rebalance()
+                                                   .assignTimestampsAndWatermarks(this.waterMark());
 
-		final DataStream<String> dataStream2 = env	.fromElements("John", "Zbe", "Abe")
-													.assignTimestampsAndWatermarks(this.waterMark());
+        final DataStream<String> refStream = env.fromElements("John", "Zbe", "Abe")
+                                                .assignTimestampsAndWatermarks(this.waterMark());
 
-		dataStream1	.join(dataStream2)
-					.where(new KeySelector<JsonNode, String>() {
+        mainStream.join(refStream)
+                  .where(new KeySelector<JsonNode, String>() {
 
-						private static final long serialVersionUID = -8244502354779754470L;
+                      private static final long serialVersionUID = -8244502354779754470L;
 
-						@Override
-						public String getKey(final JsonNode value) throws Exception {
-							System.out.println("where:value " + value);
-							return value.get("name")
-										.asText();
-						}
-					})
-					.equalTo(new KeySelector<String, String>() {
+                      @Override
+                      public String getKey(final JsonNode value) throws Exception {
+                          System.out.println("where:value " + value);
+                          return value.get("name")
+                                      .asText();
+                      }
+                  })
+                  .equalTo(new KeySelector<String, String>() {
 
-						private static final long serialVersionUID = 6569463193999775400L;
+                      private static final long serialVersionUID = 6569463193999775400L;
 
-						@Override
-						public String getKey(final String value) throws Exception {
-							System.out.println("equal:value " + value);
-							return value;
-						}
-					})
-					.window(SlidingEventTimeWindows.of(Time.minutes(50) /* size */, Time.minutes(10) /* slide */))
-					.apply(new JoinFunction<JsonNode, String, String>() {
+                      @Override
+                      public String getKey(final String value) throws Exception {
+                          System.out.println("equal:value " + value);
+                          return value;
+                      }
+                  })
+                  .window(SlidingEventTimeWindows.of(Time.minutes(50) /* size */, Time.minutes(10) /* slide */))
+                  .apply(new JoinFunction<JsonNode, String, String>() {
 
-						private static final long serialVersionUID = -7013974409091960477L;
+                      private static final long serialVersionUID = -7013974409091960477L;
 
-						@Override
-						public String join(final JsonNode first, final String second) throws Exception {
+                      @Override
+                      public String join(final JsonNode first, final String second) throws Exception {
 
-							System.out.println("########first: " + first);
-							System.out.println("###########second: " + second);
+                          System.out.println("########first: " + first);
+                          System.out.println("###########second: " + second);
 
-							return first + " " + second;
-						}
-					})
-					.print();
+                          return first + " " + second;
+                      }
+                  })
+                  .print();
 
-		env.execute();
-	}
+        env.execute();
+    }
 
-	private <T> WatermarkStrategy<T> waterMark() {
-		return new WatermarkStrategy<T>() {
+    private <T> WatermarkStrategy<T> waterMark() {
+        return new WatermarkStrategy<T>() {
 
-			private static final long serialVersionUID = 1L;
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public WatermarkGenerator<T> createWatermarkGenerator(
-					final org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier.Context context) {
-				return new AscendingTimestampsWatermarks<>();
-			}
+            @Override
+            public WatermarkGenerator<T> createWatermarkGenerator(
+                    final org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier.Context context) {
+                return new AscendingTimestampsWatermarks<>();
+            }
 
-			@Override
-			public TimestampAssigner<T> createTimestampAssigner(final TimestampAssignerSupplier.Context context) {
-				return (event, timestamp) -> System.currentTimeMillis();
-			}
+            @Override
+            public TimestampAssigner<T> createTimestampAssigner(final TimestampAssignerSupplier.Context context) {
+                return (event, timestamp) -> System.currentTimeMillis();
+            }
 
-		};
-	}
+        };
+    }
 
-	@Override
-	protected DataStream<JsonNode> analyze(final DataStream<JsonNode> dataStream) {
-		final ObjectMapper mapper = new ObjectMapper();
+    @Override
+    protected DataStream<JsonNode> analyze(final DataStream<JsonNode> dataStream) {
+        final ObjectMapper mapper = new ObjectMapper();
 
-		final Properties properties = new Properties();
-		properties.putAll(this.getConfiguration("source"));
-		properties.setProperty("topics", "analyzing-app-2-input-channel");
-		KafkaSource<JsonNode> source = null;
-		try {
-			source = SourceFactory.createKafkaSource(properties);
-		} catch (final InstantiationException e) {
-			e.printStackTrace();
-		} catch (final IllegalAccessException e) {
-			e.printStackTrace();
-		}
+        final Properties properties = new Properties();
+        properties.putAll(this.getConfiguration("source"));
+        properties.setProperty("topics", "analyzing-app-2-input-channel");
+        KafkaSource<JsonNode> source = null;
+        try {
+            source = SourceFactory.createKafkaSource(properties);
+        } catch (final InstantiationException e) {
+            e.printStackTrace();
+        } catch (final IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		dataStream.assignTimestampsAndWatermarks(new WatermarkStrategy<JsonNode>() {
+        dataStream.assignTimestampsAndWatermarks(new WatermarkStrategy<JsonNode>() {
 
-			private static final long serialVersionUID = 8162042624195674743L;
+            private static final long serialVersionUID = 8162042624195674743L;
 
-			@Override
-			public WatermarkGenerator<JsonNode> createWatermarkGenerator(
-					final org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier.Context context) {
-				return new AscendingTimestampsWatermarks<>();
-			}
+            @Override
+            public WatermarkGenerator<JsonNode> createWatermarkGenerator(
+                    final org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier.Context context) {
+                return new AscendingTimestampsWatermarks<>();
+            }
 
-			@Override
-			public TimestampAssigner<JsonNode> createTimestampAssigner(
-					final TimestampAssignerSupplier.Context context) {
-				return (event, timestamp) -> System.currentTimeMillis();
-			}
+            @Override
+            public TimestampAssigner<JsonNode> createTimestampAssigner(
+                    final TimestampAssignerSupplier.Context context) {
+                return (event, timestamp) -> System.currentTimeMillis();
+            }
 
-		});
-		final DataStream<JsonNode> dataStream2 = env.fromSource(source, new WatermarkStrategy<JsonNode>() {
+        });
+        final DataStream<JsonNode> dataStream2 = env.fromSource(source, new WatermarkStrategy<JsonNode>() {
 
-			private static final long serialVersionUID = 1L;
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public WatermarkGenerator<JsonNode> createWatermarkGenerator(
-					final org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier.Context context) {
-				return new AscendingTimestampsWatermarks<>();
-			}
+            @Override
+            public WatermarkGenerator<JsonNode> createWatermarkGenerator(
+                    final org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier.Context context) {
+                return new AscendingTimestampsWatermarks<>();
+            }
 
-			@Override
-			public TimestampAssigner<JsonNode> createTimestampAssigner(
-					final TimestampAssignerSupplier.Context context) {
-				return (event, timestamp) -> System.currentTimeMillis();
-			}
+            @Override
+            public TimestampAssigner<JsonNode> createTimestampAssigner(
+                    final TimestampAssignerSupplier.Context context) {
+                return (event, timestamp) -> System.currentTimeMillis();
+            }
 
-		}, "kafka2");
-		dataStream2.assignTimestampsAndWatermarks(new WatermarkStrategy<JsonNode>() {
+        }, "kafka2");
+        dataStream2.assignTimestampsAndWatermarks(new WatermarkStrategy<JsonNode>() {
 
-			private static final long serialVersionUID = 1L;
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public WatermarkGenerator<JsonNode> createWatermarkGenerator(
-					final org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier.Context context) {
-				return new AscendingTimestampsWatermarks<>();
-			}
+            @Override
+            public WatermarkGenerator<JsonNode> createWatermarkGenerator(
+                    final org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier.Context context) {
+                return new AscendingTimestampsWatermarks<>();
+            }
 
-			@Override
-			public TimestampAssigner<JsonNode> createTimestampAssigner(
-					final TimestampAssignerSupplier.Context context) {
-				return (event, timestamp) -> System.currentTimeMillis();
-			}
+            @Override
+            public TimestampAssigner<JsonNode> createTimestampAssigner(
+                    final TimestampAssignerSupplier.Context context) {
+                return (event, timestamp) -> System.currentTimeMillis();
+            }
 
-		});
-		return dataStream	.join(dataStream2)
-							.where(new KeySelector<JsonNode, String>() {
+        });
+        return dataStream.join(dataStream2)
+                         .where(new KeySelector<JsonNode, String>() {
 
-								private static final long serialVersionUID = 1L;
+                             private static final long serialVersionUID = 1L;
 
-								@Override
-								public String getKey(final JsonNode value) throws Exception {
-									System.out.println("where:json " + value);
-									return value.get("traceId")
-												.asText();
-								}
-							})
-							.equalTo(new KeySelector<JsonNode, String>() {
+                             @Override
+                             public String getKey(final JsonNode value) throws Exception {
+                                 System.out.println("where:json " + value);
+                                 return value.get("traceId")
+                                             .asText();
+                             }
+                         })
+                         .equalTo(new KeySelector<JsonNode, String>() {
 
-								private static final long serialVersionUID = 8170863507711599110L;
+                             private static final long serialVersionUID = 8170863507711599110L;
 
-								@Override
-								public String getKey(final JsonNode value) throws Exception {
-									System.out.println("equal:json " + value);
-									return value.get("traceId")
-												.asText();
-								}
-							})
-							.window(TumblingEventTimeWindows.of(Time.seconds(50)))
-							.apply(new JoinFunction<JsonNode, JsonNode, JsonNode>() {
+                             @Override
+                             public String getKey(final JsonNode value) throws Exception {
+                                 System.out.println("equal:json " + value);
+                                 return value.get("traceId")
+                                             .asText();
+                             }
+                         })
+                         .window(TumblingEventTimeWindows.of(Time.seconds(50)))
+                         .apply(new JoinFunction<JsonNode, JsonNode, JsonNode>() {
 
-								private static final long serialVersionUID = 8164167858060757109L;
+                             private static final long serialVersionUID = 8164167858060757109L;
 
-								@Override
-								public JsonNode join(final JsonNode first, final JsonNode second) throws Exception {
+                             @Override
+                             public JsonNode join(final JsonNode first, final JsonNode second) throws Exception {
 
-									System.out.println("########first: " + first);
-									System.out.println("###########second: " + second);
+                                 System.out.println("########first: " + first);
+                                 System.out.println("###########second: " + second);
 
-									return first;
-								}
-							})
-							.map(this.transformJsonNode(mapper));
-	}
+                                 return first;
+                             }
+                         })
+                         .map(this.transformJsonNode(mapper));
+    }
 
-	private MapFunction<JsonNode, JsonNode> transformJsonNode(final ObjectMapper mapper) {
-		return json -> {
+    private MapFunction<JsonNode, JsonNode> transformJsonNode(final ObjectMapper mapper) {
+        return json -> {
 
-			final ObjectNode mappedJson = mapper.createObjectNode();
+            final ObjectNode mappedJson = mapper.createObjectNode();
 
-			final DocumentContext jsonContext = JsonPath.parse(json.toString());
+            final DocumentContext jsonContext = JsonPath.parse(json.toString());
 
-			final String traceId = jsonContext.read("$.traceId");
-			mappedJson.put("traceId", traceId);
+            final String traceId = jsonContext.read("$.traceId");
+            mappedJson.put("traceId", traceId);
 //            mappedJson.put("eventId", jsonContext.<String>read("$.eventId"));
 //            mappedJson.put("status", jsonContext.<String>read("$.managementData.status"));
 //            mappedJson.put("eventTime", jsonContext.<String>read("$.managementData.stepsMetadata[0].startTime"));
-			log.info("Extract report record with traceId {}", traceId);
+            log.info("Extract report record with traceId {}", traceId);
 
-			return (JsonNode) mappedJson;
-		};
-	}
+            return (JsonNode) mappedJson;
+        };
+    }
 }
