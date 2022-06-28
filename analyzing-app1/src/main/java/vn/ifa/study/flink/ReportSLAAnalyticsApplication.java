672059b17b9ee9d753436e9c6addc85355c9293f
Tran Quang Minh <minh.tran.1@swisspost.com>
2022-06-28 12:15:45 +0700

    implement sample

--- analyzing-app1/src/main/java/vn/ifa/study/flink/ReportSLAAnalyticsApplication.java
+++ analyzing-app1/src/main/java/vn/ifa/study/flink/ReportSLAAnalyticsApplication.java
@@ -20,87 +20,109 @@ package vn.ifa.study.flink;
 
 import java.util.Properties;
 
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.formats.json.JsonRowSerializationSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
 
+import vn.ifa.study.flink.report.SLADataAnalyzer;
 import vn.sps.cdipp.factory.SourceFactory;
 
-/**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>
- * For a tutorial how to write a Flink application, check the tutorials and
- * examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>
- * To package your application into a JAR file for execution, run 'mvn clean
- * package' on the command line.
- *
- * <p>
- * If you change the name of the main class (with the public static void
- * main(String[] args)) method, change the respective entry in the POM.xml file
- * (simply search for 'mainClass').
- */
 public class ReportSLAAnalyticsApplication {
 
-	public static void main(final String[] args) throws Exception {
-//		final SLADataAnalyzer analyzer = new SLADataAnalyzer(args);
-//		analyzer.analyze();
-		
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
-		/*
+    public static void main(final String[] args) throws Exception {
+		final SLADataAnalyzer analyzer = new SLADataAnalyzer(args);
+		analyzer.analyze();
+//        test();
+    }
+
+    private static void test() throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+        /*
          * Create kafka source table
          */
-		tableEnv.executeSql(
-                String.join(
-                        "\n",
-                        "CREATE TABLE kafkaTable (",
-                        "  name STRING,",
-                        "  birthYear BIGINT,",
-                        "  file ARRAY< ROW<startTime STRING, endTime STRING> >,",
-                        "  ts TIMESTAMP_LTZ(3) METADATA FROM 'timestamp',",
-                        "  WATERMARK FOR ts AS ts",
-                        ") WITH (",
-                        "  'connector' = 'kafka',",
-                        "  'topic' = 'analyzing-name-input-channel',",
+        TableResult tableResult = tableEnv.executeSql(
+                String.join("\n", "CREATE TABLE kafkaTable (", "  eventId STRING,",
+                        "  managementData ROW< stepsMetadata ARRAY< ROW<startTime STRING, endTime STRING> > >,",
+                        "  ts TIMESTAMP_LTZ(3) METADATA FROM 'timestamp',", "  WATERMARK FOR ts AS ts", ") WITH (",
+                        "  'connector' = 'kafka',", "  'topic' = 'analyzing-name-input-channel',",
                         "  'properties.bootstrap.servers' = '10.10.15.85:32092',",
 //                        "  'scan.startup.mode' = 'earliest-offset',",
-                        "  'format' = 'json'",
-                        ")"));
-		tableEnv.from("kafkaTable").execute().print();
-		/*
+                        "  'format' = 'json'", ")"));
+
+//		tableEnv.executeSql(String.join(
+//		        "\n",
+//                "CREATE TABLE firehoseTable ("
+//                , "  startTime STRING,"
+//                , "  endTime STRING,"
+//                , "  eventID STRING"
+//                , ") WITH ("
+//                , "  'connector' = 'firehose',"
+//                , "  'delivery-stream' = 'report-demo-data',"
+//                , "  'aws.region' = 'eu-central-1',"
+//                , "  'aws.credentials.basic.accesskeyid' = 'AKIATV2PQP5GPBZLMAVC',"
+//                , "  'aws.credentials.basic.secretkey' = 'eMJEfz1gbg2zwmxwBRPyexA4JWeFYgnOvSSBeI,l',"
+//                , "  'format' = 'json'"
+//                , ")"));
+//		tableEnv.from("kafkaTable").execute().print();
+
+        /*
          * Create a s3 data stream
          */
-		final Properties properties = new Properties();
-		properties.put("filePath", "file:///C:/Users/tqminh_1/Downloads/test");
-		properties.put("numberOfColumns", "3");
-		properties.put("fileProcessingMode", "PROCESS_CONTINUOUSLY");
-		properties.put("intervalCheck", "1000");
-		
-		final DataStreamSource<Row> s3DataStream = SourceFactory.createS3Source(properties, env);
-		s3DataStream.print("s3-source--");
-		/*
+        final Properties properties = new Properties();
+        properties.put("filePath", "file:///C:/Users/tqminh_1/Downloads/test");
+        properties.put("numberOfColumns", "3");
+        properties.put("fileProcessingMode", "PROCESS_CONTINUOUSLY");
+        properties.put("intervalCheck", "1000");
+
+        final DataStreamSource<Row> s3DataStream = SourceFactory.createS3Source(properties, env);
+        /*
          * Convert data stream to table
          */
-		final Schema userInfoSchema = Schema.newBuilder()
-				.column("f0", "STRING NOT NULL")
-				.column("f1", "STRING NOT NULL")
-				.column("f2", "STRING NOT NULL")
-				.primaryKey("f0")
-				.build();
-		final Table userInfoTable = tableEnv.fromDataStream(s3DataStream, userInfoSchema);
-		tableEnv.createTemporaryView("userInfoTable", userInfoTable);
-		/*
+        final Schema userInfoSchema = Schema.newBuilder()
+                                            .column("f0", "STRING NOT NULL")
+                                            .column("f1", "STRING NOT NULL")
+                                            .column("f2", "STRING NOT NULL")
+                                            .primaryKey("f0")
+                                            .build();
+        final Table userInfoTable = tableEnv.fromDataStream(s3DataStream, userInfoSchema);
+        tableEnv.createTemporaryView("userInfoTable", userInfoTable);
+        /*
          * Execute sql
          */
-		Table enrichedTable = tableEnv.sqlQuery("SELECT * "
-				+ "FROM userInfoTable userInfo JOIN kafkaTable kafka ON userInfo.f1 = kafka.name");
-		enrichedTable.execute().print();
-		env.execute();
-	}
+//		Table enrichedTable = tableEnv.sqlQuery("SELECT * "
+//				+ "FROM userInfoTable userInfo JOIN kafkaTable kafka ON userInfo.f1 = kafka.name");
+
+        Table enrichedTable = tableEnv.sqlQuery("SELECT managementData.stepsMetadata[1].startTime as startTime, "
+                + "managementData.stepsMetadata[1].endTime as endTime " + "FROM kafkaTable");
+
+//		enrichedTable.executeInsert("firehoseTable");
+
+        final Properties kafkaproperties = new Properties();
+        kafkaproperties.put("bootstrap.servers", "10.10.15.85:32092");
+        kafkaproperties.put("topic", "analyzing-app-1-output-channel");
+
+        DataStream<Row> dataStream = tableEnv.toDataStream(enrichedTable);
+        String[] str = new String[] {"startTime"};
+        TypeInformation[] informations = new TypeInformation[] {BasicTypeInfo.STRING_TYPE_INFO};
+        TypeInformation information = new RowTypeInfo(informations, str);
+
+        FlinkKafkaProducer flinkKafkaProducer = new FlinkKafkaProducer<>("analyzing-app-1-output-channel",
+                new JsonRowSerializationSchema.Builder(information).build(), kafkaproperties);
+
+        dataStream.addSink(flinkKafkaProducer);
+
+        dataStream.print();
+        env.execute();
+    }
 }
--- analyzing-app1/src/main/java/vn/ifa/study/flink/report/SLADataAnalyzer.java
+++ analyzing-app1/src/main/java/vn/ifa/study/flink/report/SLADataAnalyzer.java
@@ -15,250 +15,84 @@ package vn.ifa.study.flink.report;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.apache.flink.api.common.eventtime.AscendingTimestampsWatermarks;
-import org.apache.flink.api.common.eventtime.RecordTimestampAssigner;
-import org.apache.flink.api.common.eventtime.TimestampAssigner;
-import org.apache.flink.api.common.eventtime.TimestampAssignerSupplier;
-import org.apache.flink.api.common.eventtime.WatermarkGenerator;
-import org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.functions.co.CoProcessFunction;
-import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.table.api.Expressions;
 import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableResult;
-import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.types.Row;
-import org.apache.flink.util.CloseableIterator;
-import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-
-import vn.sps.cdipp.AbstractDataAnalyzer;
+import vn.sps.cdipp.AbstractTableAnalyzer;
 import vn.sps.cdipp.factory.SourceFactory;
 
-public class SLADataAnalyzer extends AbstractDataAnalyzer<JsonNode> {
+public class SLADataAnalyzer extends AbstractTableAnalyzer {
 
-	private static final Logger log = LoggerFactory.getLogger(SLADataAnalyzer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SLADataAnalyzer.class);
 
-	private static final long serialVersionUID = 4919141782930956120L;
-	
-	public SLADataAnalyzer(final String[] args) throws IOException {
-		super(args);
-	}
+    private static final long serialVersionUID = 4919141782930956120L;
 
-	@Override
-	protected DataStream<JsonNode> analyze(final DataStream<JsonNode> dataStream) {
-        /*
-         * Create two data streams
-         */
-		final DataStream<JsonNode> dataStream1 = dataStream
-				.assignTimestampsAndWatermarks(IngestionTimeWatermarkStrategy.<JsonNode>create());
-		final Properties properties = this.getConfiguration("userInfo");
-		final DataStreamSource<Row> dataStream2 = SourceFactory.createS3Source(properties,
-				getStreamExecutionEnvironment());
-		
-		/*
-         * Convert data stream to table
-         */
-		final StreamTableEnvironment tableEnv = getStreamTableEnvironment();
-		
-		final Schema userInfoSchema = Schema.newBuilder()
-				.column("f0", "STRING NOT NULL")
-				.column("f1", "STRING NOT NULL")
-				.column("f2", "STRING NOT NULL")
-				.primaryKey("f0")
-				.build();
-		
-		final Table userInfoTable = tableEnv.fromChangelogStream(dataStream2, userInfoSchema);
-		tableEnv.createTemporaryView("userInfoTable", userInfoTable);
+    public SLADataAnalyzer(final String[] args) throws IOException {
+        super(args);
+    }
 
-		final Schema kafkaSchema = Schema.newBuilder()
-				.column("f0", "STRING")
-				.build();
-		
-		final Table kafkaTable = tableEnv.fromDataStream(dataStream1, kafkaSchema);
-		kafkaTable.execute();
-//		tableEnv.createTemporaryView("kafkaTable", kafkaTable);
-//		
-//		getStreamTableEnvironment().executeSql("SELECT * FROM kafkaTable").print();
-		
-		/*
-         * Enrich the user name json
-         */
-		
-//		Table enrichedTable = getStreamTableEnvironment().sqlQuery("SELECT userInfo.f1 , kafka.f FROM userInfoTable userInfo JOIN kafkaTable kafka ON userInfo.f1 = kafka.f0");
-		
-//		dataStream1
-//			.map(enrichUserNameJson())
-//			.print("After joinning - ");
-		
-//		dataStream1
-//			.join(dataStream2)
-//			.where(getDataStream1Key("where"))
-//			.equalTo(getDataStream1Key("equalTo"))
-//			.window(TumblingEventTimeWindows.of(Time.seconds(5)))
-//			.apply(new JoinFunction<JsonNode, JsonNode, String>() {
-//	
-//				private static final long serialVersionUID = -7013974409091960477L;
-//	
-//				@Override
-//				public String join(JsonNode first, JsonNode second) throws Exception {
-//					
-//					System.out.println("########first: " + first);
-//					System.out.println("###########second: " + second);
-//					
-//					return first+" "+second;
-//				}
-//			})
-//			.print();
-		return dataStream;
-	}
+    @Override
+    protected String sourceTableStatement() {
+        return "CREATE TABLE kafkaTable (eventId STRING, "
+                + "username STRING, "
+                + "managementData ROW< stepsMetadata ARRAY< ROW<startTime STRING, endTime STRING> > >, "
+                + "ts TIMESTAMP_LTZ(3) METADATA FROM 'timestamp', " + "WATERMARK FOR ts AS ts)";
+    }
 
-	private MapFunction<JsonNode, JsonNode> enrichUserNameJson() {
-		return new MapFunction<JsonNode, JsonNode>() {
+    @Override
+    protected RowTypeInfo rowTypeInfo() {
 
-			private static final long serialVersionUID = -7826288729532687462L;
+        final TypeInformation<?>[] typeInformations = new TypeInformation[3];
+        typeInformations[0] = BasicTypeInfo.STRING_TYPE_INFO;
+        typeInformations[1] = BasicTypeInfo.STRING_TYPE_INFO;
+        typeInformations[2] = BasicTypeInfo.STRING_TYPE_INFO;
 
-			@Override
-			public JsonNode map(JsonNode value) throws Exception {
-				
-				final String name = value.get("name").asText();
-				
-				final TableResult joinedUserInfoTableResult = getStreamTableEnvironment()
-						.sqlQuery(String.format("SELECT * FROM userInfoTable WHERE f1 = '%s'", name))
-						.execute();
-				
-				final CloseableIterator<Row> iter = joinedUserInfoTableResult.collect();
-				
-				String fullName = null;
-				while (iter.hasNext()) {
-					final Row row = (Row) iter.next();
-					fullName = (String) row.getField("fullName");
-				}
-				
-				final ObjectNode objectNode = (ObjectNode) value; 
-				objectNode.put("fullName", fullName);
-				objectNode.toPrettyString();
-				
-				return objectNode;
-			}
-		};
-	}
+        final String[] fieldNames = new String[3];
+        fieldNames[0] = "startTime";
+        fieldNames[1] = "endTime";
+        fieldNames[2] = "name";
 
-	private CoProcessFunction<JsonNode, Row, String> lookupFullName() {
-		return new CoProcessFunction<JsonNode, Row, String>() {
+        RowTypeInfo rowTypeInfo = new RowTypeInfo(typeInformations, fieldNames);
 
-			private ValueState<Row> referenceDataState = null;
-			
-			private static final long serialVersionUID = -6905557599249605381L;
+        return rowTypeInfo;
+    }
 
-			@Override
-			public void open(Configuration config) {
-				
-				System.out.println("Open function is running...!");
-				
-				ValueStateDescriptor<Row> cDescriptor = new ValueStateDescriptor<>(
-						"referenceData",
-						TypeInformation.of(Row.class)
-				);
-				referenceDataState = getRuntimeContext().getState(cDescriptor);
-			}
-			
+    @Override
+    protected Table analyzeData() {
+        Table sourceTable = getStreamTableEnvironment().from("kafkaTable");
 
-			@Override
-			public void processElement1(JsonNode jsonKafka, CoProcessFunction<JsonNode, Row, String>.Context arg1,
-					Collector<String> output) throws Exception {
-				System.out.println("get name: " + referenceDataState.value());
-				output.collect(jsonKafka + " with " + referenceDataState.value());
-			}
+        final Properties properties = this.getConfiguration("userInfo");
 
-			@Override
-			public void processElement2(Row row, CoProcessFunction<JsonNode, Row, String>.Context arg1,
-					Collector<String> arg2) throws Exception {
-				
-				System.out.println("update name: " + row);
-				referenceDataState.update(row);
-			}
+        final Schema userInfoSchema = Schema.newBuilder()
+                                            .column("f0", "STRING NOT NULL")
+                                            .column("f1", "STRING NOT NULL")
+                                            .column("f2", "STRING NOT NULL")
+                                            .primaryKey("f0")
+                                            .build();
+        SourceFactory.createTableFromS3DataStream("userInfoTable", userInfoSchema, properties,
+                getStreamExecutionEnvironment(), getStreamTableEnvironment());
 
-		};
-	}
+        Table userInfo = getStreamTableEnvironment().from("userInfoTable");
 
-	private KeySelector<Row, String> getDataStream2Key() {
-		return new KeySelector<Row, String>() {
-					private static final long serialVersionUID = -8244502354779754470L;
-
-					@Override
-					public String getKey(Row value) throws Exception {
-						System.out.println("connect:value " + value.getField(1));
-						return (String) value.getField(1);
-					}
-				};
-	}
-
-	private KeySelector<JsonNode, String> getDataStream1Key(String functionName) {
-		return new KeySelector<JsonNode, String>() {
-			private static final long serialVersionUID = -8244502354779754470L;
-
-			@Override
-			public String getKey(JsonNode value) throws Exception {
-				System.out.println(functionName + ":value " + value.get("name").asText());
-				return value.get("name").asText();
-			}
-		};
-	}
-
-	private MapFunction<JsonNode, JsonNode> transformJsonNode(final ObjectMapper mapper) {
-		return json -> {
-			final ObjectNode mappedJson = mapper.createObjectNode();
-
-			final DocumentContext jsonContext = JsonPath.parse(json.toString());
-
-			final String traceId = jsonContext.read("$.traceId");
-			mappedJson.put("traceId", traceId);
-//            mappedJson.put("eventId", jsonContext.<String>read("$.eventId"));
-//            mappedJson.put("status", jsonContext.<String>read("$.managementData.status"));
-//            mappedJson.put("eventTime", jsonContext.<String>read("$.managementData.stepsMetadata[0].startTime"));
-			log.info("Extract report record with traceId {}", traceId);
-
-			return (JsonNode) mappedJson;
-		};
-	}
-	
-    private static class IngestionTimeWatermarkStrategy<T> implements WatermarkStrategy<T> {
-
-		private static final long serialVersionUID = 2099042171426356537L;
-
-		private IngestionTimeWatermarkStrategy() {}
-
-        public static <T> IngestionTimeWatermarkStrategy<T> create() {
-            return new IngestionTimeWatermarkStrategy<>();
-        }
-
-        @Override
-        public WatermarkGenerator<T> createWatermarkGenerator(
-                WatermarkGeneratorSupplier.Context context) {
-            return new AscendingTimestampsWatermarks<>();
-        }
-
-        @Override
-        public TimestampAssigner<T> createTimestampAssigner(
-                TimestampAssignerSupplier.Context context) {
-            return new RecordTimestampAssigner<T>();
-        }
+        Table result = sourceTable.join(userInfo)
+                                  .where(Expressions.$("username")
+                                                    .isEqual(Expressions.$("f1")))
+                                  .select(Expressions.$("managementData")
+                                                     .get("stepsMetadata")
+                                                     .at(1)
+                                                     .get("startTime"),
+                                          Expressions.$("managementData")
+                                                     .get("stepsMetadata")
+                                                     .at(1)
+                                                     .get("endTime"),
+                                          Expressions.$("f2"));
+        return result;
     }
 }
